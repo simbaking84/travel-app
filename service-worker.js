594c9b0d658baf50c5167d5552d8a65c1932b766
simbaking84 <moriaty84@gmail.com>
2026-05-27 23:55:08 +0900

    sw v10 → v11

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('message',event=>{
   if(event.data&&event.data.type==='SKIP_WAITING')self.skipWaiting();
-});const CACHE_NAME = 'travel-planner-v10';
+});const CACHE_NAME = 'travel-planner-v11';
 const STATIC_ASSETS = [
   './',
   './index.html',
